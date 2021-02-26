3a74ac703af0827da5f2a1b741edb978c9c96d49
Bair_Work <aveevb123@mail.ru>
2021-02-26 17:45:47 +0300

    added script file

---
+++ js/script.js
@@ -0,0 +1,6 @@
+"use strict";
+document.querySelector(".header__burger").addEventListener("clic", function() {
+    const burgerListener = document.querySelector(".header__burger--active");
+    
+    burgerListener ? 
+})
