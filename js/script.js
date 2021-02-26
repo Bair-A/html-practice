9fdae5c9f5ba5d37e4a7c360e79becc5480f4d42
Bair Aveev <aveevb123@gmail.com>
2021-02-27 00:17:19 +0300

    added burher menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
-document.querySelector(".header__burger").addEventListener("clic", function() {
-    const burgerListener = document.querySelector(".header__burger--active");
-    
-    burgerListener ? 
+document.querySelector(".header__burger").addEventListener("click", () => {
+    document.querySelector(".header__burger").classList.toggle("header__burger--active");
+    document.querySelector("body").classList.toggle("body--burger-active");   
+    document.querySelector(".header__menu").classList.toggle("header__menu--active");
 })
