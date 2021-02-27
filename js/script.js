71704eb4601a1d1a074c239393dc568a3020bbb1
Bair Aveev <aveevb123@gmail.com>
2021-02-27 23:04:19 +0300

    added touchend

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 "use strict";
 const headerBurger = document.querySelector(".header__burger")
 
-headerBurger.addEventListener("click", () => {
+// headerBurger.addEventListener("click", () => {
     
-    headerBurger.classList.toggle("header__burger--active");
-    document.querySelector("body").classList.toggle("body--burger-active");   
-    document.querySelector(".header__menu").classList.toggle("header__menu--active");
-})
+//     headerBurger.classList.toggle("header__burger--active");
+//     document.querySelector("body").classList.toggle("body--burger-active");   
+//     document.querySelector(".header__menu").classList.toggle("header__menu--active");
+// })
 
 
 headerBurger.addEventListener("touchend", () => {
