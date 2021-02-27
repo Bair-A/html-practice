f01c576037e5207fb5ef382e30938406cb9791be
Bair Aveev <aveevb123@gmail.com>
2021-02-27 23:17:36 +0300

    returnet eventListener click

--- js/script.js
+++ js/script.js
@@ -1,17 +1,11 @@
 "use strict";
 const headerBurger = document.querySelector(".header__burger")
 
-// headerBurger.addEventListener("click", () => {
-    
-//     headerBurger.classList.toggle("header__burger--active");
-//     document.querySelector("body").classList.toggle("body--burger-active");   
-//     document.querySelector(".header__menu").classList.toggle("header__menu--active");
-// })
-
-
-headerBurger.addEventListener("touchend", () => {
+headerBurger.addEventListener("click", () => {
     
     headerBurger.classList.toggle("header__burger--active");
     document.querySelector("body").classList.toggle("body--burger-active");   
     document.querySelector(".header__menu").classList.toggle("header__menu--active");
 })
+
+
